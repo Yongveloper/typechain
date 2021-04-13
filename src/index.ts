1d86452cda45127b8e36a8a326d205ce42cbf3c5
Yongveloper <aaa9015@naver.com>
2021-04-13 19:30:57 +0900

    #7 Blockchain Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender?: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const yong = new Human('yong', 23, 'male');
+const genesisBlock: Block = new Block(0, '123134', '', 'Hello', 123456);
 
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
-}
+let blockChain: [Block] = [genesisBlock];
 
-const sayHi = (person: Human): string => {
-  const { name, age, gender } = person;
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
-};
-
-console.log(sayHi(yong));
+console.log(blockChain);
 
 export {};
