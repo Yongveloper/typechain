c61d1e29f9e1be4f6344275b571f574dca308192
Yongveloper <aaa9015@naver.com>
2021-04-13 20:14:03 +0900

    #10 Vaildating Block Structure

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
 import * as CryptoJS from 'crypto-js';
 
 class Block {
-  public index: number;
-  public hash: string;
-  public previousHash: string;
-  public data: string;
-  public timestamp: number;
-
   static calculateBlockHash = (
     index: number,
     previousHash: string,
@@ -14,6 +8,19 @@ class Block {
     data: string
   ): string =>
     CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  static vaildateStructure = (aBlock: Block): boolean =>
+    typeof aBlock.index === 'number' &&
+    typeof aBlock.hash === 'string' &&
+    typeof aBlock.previousHash === 'string' &&
+    typeof aBlock.timestamp === 'number' &&
+    typeof aBlock.data === 'string';
+
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
 
   constructor(
     index: number,
@@ -41,19 +48,19 @@ const getLatesBlock = (): Block => blockchain[blockchain.length - 1];
 const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
 
 const createNewBlock = (data: string): Block => {
-  const previosBlock: Block = getLatesBlock();
-  const newIndex: number = previosBlock.index + 1;
+  const previousBlock: Block = getLatesBlock();
+  const newIndex: number = previousBlock.index + 1;
   const newTimestamp: number = getNewTimestamp();
   const newHash: string = Block.calculateBlockHash(
     newIndex,
-    previosBlock.hash,
+    previousBlock.hash,
     newTimestamp,
     data
   );
   const newBlock: Block = new Block(
     newIndex,
     newHash,
-    previosBlock.hash,
+    previousBlock.hash,
     data,
     newTimestamp
   );
@@ -61,5 +68,15 @@ const createNewBlock = (data: string): Block => {
 };
 
 console.log(createNewBlock('hello'), createNewBlock('bye bye'));
+
+const isBlockVaild = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.vaildateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  }
+};
 
 export {};
