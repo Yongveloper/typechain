59452083b0b3e34ede5930a7bc1a8079b31e10fc
Yongveloper <aaa9015@naver.com>
2021-04-13 19:19:26 +0900

    #6 Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender?: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const yong = new Human('yong', 23, 'male');
+
 interface Human {
   name: string;
   age: number;
   gender: string;
 }
 
-const person = {
-  name: 'yong',
-  age: 23,
-  gender: 'male',
-};
-
 const sayHi = (person: Human): string => {
   const { name, age, gender } = person;
   return `Hello ${name}, you are ${age}, you are a ${gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(yong));
 
 export {};
