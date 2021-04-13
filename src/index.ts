e7615d41ba4b769af6d3e5c7682d7d95bdd46933
Yongveloper <aaa9015@naver.com>
2021-04-13 19:58:05 +0900

    #9 Creating a Block part Three

--- src/index.ts
+++ src/index.ts
@@ -40,4 +40,26 @@ const getLatesBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
 
+const createNewBlock = (data: string): Block => {
+  const previosBlock: Block = getLatesBlock();
+  const newIndex: number = previosBlock.index + 1;
+  const newTimestamp: number = getNewTimestamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previosBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previosBlock.hash,
+    data,
+    newTimestamp
+  );
+  return newBlock;
+};
+
+console.log(createNewBlock('hello'), createNewBlock('bye bye'));
+
 export {};
